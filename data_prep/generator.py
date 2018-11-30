a6f58d8e5eb247c09aefcd73aecb8c1eda16b137
fmasche <fmasche@gmail.com>
2018-11-30 16:47:17 -0500

    Adding new training file

--- data_prep/generator.py
+++ data_prep/generator.py
@@ -84,7 +84,8 @@ def generate_data(original_queries_filename):
     # Create a Pandas dataframe from some data.
     df = pd.DataFrame(newRows, columns=columns)
     # Create a Pandas Excel writer using XlsxWriter as the engine.
-    writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
+    newFilename = 'training_data_' + '3' + '.xlsx'
+    writer = pd.ExcelWriter(newFilename, engine='xlsxwriter')
     # Convert the dataframe to an XlsxWriter Excel object.
     df.to_excel(writer, sheet_name='Sheet1')
     # Close the Pandas Excel writer and output the Excel file.
--- data_prep/loaders.py
+++ data_prep/loaders.py
@@ -3,33 +3,83 @@ import numpy as np
 import pandas
 import preprocessing
 
-DEFAULT_FILENAME = "files/training_data_1.xlsx"
+DEFAULT_FILENAME = "files/training_data_3.xlsx"
 
 def load_data(filename):
     xArr = []
     yArr = []
+    xTestArr = []
+    yTestArr = []
+
     excepCount = 0
     data = dict()   
-    df = pandas.read_excel(open(filename,'rb'), sheet_name=0)    
+    df = pandas.read_excel(open(filename,'rb'), sheet_name=0)   # training
     for i in df.index:
         data["text"] = df['query'][i]
         data["rows_examined"] = df['rows'][i]
-        role = df['role2'][i]
+        role = df['role'][i]
         if (data["text"] == "query"):  # only to ignore the first line, TODO should change this
             continue
         try:
-            features = preprocessing.getFeaturesFromQuery(data)
+            features = preprocessing.getFeaturesFromDBSAFE(data)
             #print(features)
             xArr.append(features[0])
             yArr.append(role)
         except Exception as error:
             print(error)
             excepCount = excepCount + 1
+
+    df = pandas.read_excel(open(filename,'rb'), sheet_name=0)   # test
+    for i in df.index:
+        data["text"] = df['query'][i]
+        data["rows_examined"] = df['rows'][i]
+        role = df['role'][i]
+        if (data["text"] == "query"):  # only to ignore the first line, TODO should change this
+            continue
+        try:
+            features = preprocessing.getFeaturesFromDBSAFE(data)
+            #print(features)
+            xTestArr.append(features[0])
+            yTestArr.append(role)
+        except Exception as error:
+            print(error)
+            excepCount = excepCount + 1
+
+    return np.array(xArr), np.array(yArr), np.array(xTestArr), np.array(yTestArr)
+
+
+def load_training_test_data(filename):
+    xTraining = list()
+    yTraining = list()
+    bow = list()
+    xTest = list()
+    yTest = list()
+
+    df = pandas.read_excel(open(filename,'rb'), sheet_name=0)       # training
+    for i in df.index:
+        if (df['query'][i] == "query"):  # only to ignore the first line, TODO should change this
+            continue
+        try:
+            xTraining.append([df['query'][i], df['rows'][i]])
+            yTraining.append(df['role'][i])
+
+            bow.append(df['query'][i])
+        except Exception as error:
+            print(error)
+            pass
+
+    df = pandas.read_excel(open(filename,'rb'), sheet_name=2)       # test
+    for i in df.index:
+        if (df['query'][i] == "query"):  # only to ignore the first line, TODO should change this
+            continue
+        try:
+            xTest.append([df['query'][i], df['rows'][i]])
+            yTest.append(df['role'][i])
+        except Exception as error:
+            print(error)
+            pass            
             
-    xs = np.array(xArr)
-    ys = np.array(yArr)
-    print("xs shape: ", xs.shape)     
-    return xs, ys
+    return np.array(xTraining), np.array(yTraining), np.array(xTest), np.array(yTest), np.array(bow)
 
 
 if __name__ == "__main__":
