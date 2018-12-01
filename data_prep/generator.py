55d5ddce99ca469935737d66dd262a27b2b6ffe0
fmasche <fmasche@gmail.com>
2018-12-01 13:24:03 -0500

    Adding bag-of-words features

--- data_prep/generator.py
+++ data_prep/generator.py
@@ -42,43 +42,48 @@ def generate_data(original_queries_filename):
     originalHR = list()
     originalDoc = list()
     originalAdm = list()
-    originalPent = list()
+    originalPent = list()   # the key is the query, the value is the original role
 
     totalSamples = 12000
-    maliciousSamples = (5*totalSamples) / 100
+    maliciousSamples = (5*totalSamples) / 100       # 5% of our samples are maliciuos
     trueSamples = totalSamples - maliciousSamples
-    columns = ['query', 'role', 'valid', 'rows']
+    columns = ['query', 'role1', 'valid', 'role2', 'rows']
 
-    sheet1 = pd.read_excel(open(original_queries_filename,'rb'), sheet_name=1)
+    sheet1 = pd.read_excel(open(original_queries_filename,'rb'), sheet_name=1)  # 'Original' tab
     for i in sheet1.index:
         query = sheet1['query'][i]
         if (query == "query"):  # only to ignore the first line, TODO should change this
             continue
-        role = sheet1['role'][i]
+        role1 = sheet1['role1'][i]
         valid = sheet1['valid'][i]
+        role2 = sheet1['role2'][i]
+        rows = sheet1['rows'][i]
 
         if (valid == 'Y'):
-            if (role == 'HR'):
+            if (role2 == 'HR'):
                 originalHR.append(query)
-            elif (role == 'DOC'):
+            elif (role2 == 'DOC'):
                 originalDoc.append(query)
-            elif (role == 'ADM'):
+            elif (role2 == 'ADM'):
                 originalAdm.append(query)
         else:
-            originalPent.append(query)
+            data = { "query": query, "role1": role1, "rows": rows }
+            originalPent.append(data)
 
     newRows = []
     for i in range(trueSamples):
         randomRole = random.choice(VALID_ROLES)
         if (randomRole == 'HR'):
-            newRows.append((random.choice(originalHR), 'HR', 'Y', random.randint(1,21)))
+            newRows.append((random.choice(originalHR), 'HR', 'Y', 'HR', random.randint(1,21)))
         elif (randomRole == 'DOC'):
-            newRows.append((random.choice(originalDoc), 'DOC', 'Y', random.randint(1,21)))
+            newRows.append((random.choice(originalDoc), 'DOC', 'Y', 'DOC', random.randint(1,21)))
         elif (randomRole == 'ADM'):
-            newRows.append((random.choice(originalAdm), 'ADM', 'Y', random.randint(1,21)))
+            newRows.append((random.choice(originalAdm), 'ADM', 'Y', 'ADM', random.randint(1,21)))
 
     for i in range(maliciousSamples):
-        newRows.append((random.choice(originalPent), 'PENT','N', random.randint(1,300024)))
+        rdmPent = random.choice(originalPent)
+        max_rows = rdmPent["rows"]
+        newRows.append((rdmPent["query"], rdmPent["role1"],'N', 'PENT', random.randint(1,max_rows)))
 
     #print(newRows)
     # Create a Pandas dataframe from some data.
@@ -91,12 +96,24 @@ def generate_data(original_queries_filename):
     # Close the Pandas Excel writer and output the Excel file.
     writer.save()
 
+def get_BOW(original_queries_filename):
+    retVal = list()
 
+    sheet1 = pd.read_excel(open(original_queries_filename,'rb'), sheet_name=1)  # 'Original' tab
+    for i in sheet1.index:
+        query = sheet1['query'][i]
+        if (query == "query"):  # only to ignore the first line, TODO should change this
+            continue
+        
+        query = query.replace("'", "")
+        query = query.replace("\"", "")
+        retVal.append(query)
 
-
-
+    print(retVal)
 
 
 if __name__ == "__main__":
     print('Testing generation functions..')
-    generate_data(DEFAULT_FILENAME)
+    #generate_data(DEFAULT_FILENAME)
+    get_BOW(DEFAULT_FILENAME)
+    print 'Generation done. [', DEFAULT_FILENAME, ']'
--- data_prep/preprocessing.py
+++ data_prep/preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 from os import listdir, path
 import constants
+from sklearn.feature_extraction.text import CountVectorizer
 
 TEST_QUERY_SELECT = "select employees.first_name, employees.last_name, employees.gender, employees.birth_date, employees.hire_date, salaries.salary from employees inner join salaries on employees.emp_no = salaries.emp_no where employees.emp_no = @param;"
 TEST_QUERY_SELECT_ALL = "select * from salaries where salary > @param;"
@@ -17,6 +18,8 @@ TEST_QUERY_UPDATE = "update billing set amount = @param where billing_no = @para
 TEST_QUERY_UPDATE_WO_WHERE = "update billing set amount = @param;"
 TEST_QUERY_DELETE = "delete from patient where patient_no = @param;"
 TEST_QUERY_DELETE_WO_WHERE = "delete from billing;"
+
+BOW = "select employees.emp_no from employees where employees.first_name like @param%; select employees.emp_no from employees where employees.last_name like @param%; select employees.first_name employees.last_name employees.gender employees.birth_date employees.hire_date salaries.salary from employees inner join salaries on employees.emp_no = salaries.emp_no where employees.emp_no = @param; select departments.dept_no departments.dept_name from departments; select employees.emp_no employees.first_name employees.last_name dept_emp.dept_no dept_emp.from_date dept_emp.to_date from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no where employees.emp_no = @param; select employees.emp_no employees.first_name employees.last_name titles.title titles.from_date titles.to_date from employees inner join titles on employees.emp_no = titles.emp_no where employees.emp_no = @param; insert into departments values (); insert into employees values (); insert into salaries values (); insert into dept_emp values (); select * from employees; select * from employees where birth_date between  and ; select * from employees where hire_date between  and ; select * from employees where gender = @param; select * from salaries; select * from salaries where salary > @param; select * from salaries where salary < @param; select * from salaries where salary = @param; select * from dept_emp; delete from employees; delete from salaries; delete from dept_emp; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.last_name like @param; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.first_name like @param; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.patient_no = @param; select patient.patient_no patient.first_name patient.last_name patient.birth_date prescribes.prescription_no prescribes.physician_no prescribes.dose prescribes.med_code from patient inner join prescribes on patient.patient_no = prescribes.patient_no where patient.patient_no = @param; select medication.med_code medication.name medication.brand medication.description from medication where medication.name like %param; select medication.med_code medication.name medication.brand medication.description from medication where medication.brand like %param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.patient_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.physician_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.nurse_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.start_dt_time = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient p on appointment.patient_no = patient.patient_no where appointment.examination_room = @param; select procedures.proc_code procedures.description procedures.cost from procedures where procedures.proc_code = @param; select procedures.proc_code procedures.description procedures.cost from procedures where procedures.description like %@param; insert into prescribes values (); insert into patient values (); insert into medication values (); insert into appointment values (); delete from prescribes where prescription_no = @param; delete from medication where med_code = @param; delete from patient where patient_no = @param; delete from appointment where appointment_id = @param; select * from patient; select * from patient where birth_date = @param; select first_name last_name ssn phone address birth_date from patient; select first_name last_name ssn birth_date from patient; select * from prescribes; select * from medication; select * from appointment; delete from patient; delete from prescribes; delete from medication; delete from appointment; select billing.billing_no billing.payment_method billing.amount billing.cc_number patient.patient_no patient.first_name patient.last_name patient.gender patient.insurance_id patient.phone patient.address from billing inner join patient on billing.patient_no = patient.patient_no where patient.patient_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.patient_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.physician_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.nurse_no = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.start_dt_time = @param; select appointment.appointment_id appointment.patient_no patient.first_name patient.last_name patient.birth_date appointment.nurse_no appointment.physician_no appointment.start_dt_time appointment.end_dt_time from appointment inner join patient on appointment.patient_no = patient.patient_no where appointment.examination_room = @param; select procedures.proc_code procedures.description procedures.cost from procedures where procedures.proc_code = @param; select procedures.proc_code procedures.description procedures.cost from procedures where procedures.description like %@param; insert into billing values (); insert into appointment values (); insert into procedures values (); update billing set amount = @param where billing_no = @param; update billing set payment_method = @param cc_number = @param where billing_no = @param; update procedures set cost = @param where proc_code = @param; delete from billing where billing_no = @param; delete from billing where patient_no = @param; delete from procedures where proc_code = @param; select * from appointment; select * from billing; select * from billing where amount = @param; delete from billing; delete from appointment; delete from procedures; select employees.emp_no from employees where employees.first_name like @param%; select employees.emp_no from employees where employees.last_name like @param%; select departments.dept_no departments.dept_name from departments; select employees.emp_no employees.first_name employees.last_name dept_emp.dept_no dept_emp.from_date dept_emp.to_date from employees inner join dept_emp on employees.emp_no = dept_emp.emp_no where employees.emp_no = @param; select employees.emp_no employees.first_name employees.last_name titles.title titles.from_date titles.to_date from employees inner join titles on employees.emp_no = titles.emp_no where employees.emp_no = @param; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.last_name like @param; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.first_name like @param; select patient.patient_no patient.birth_date patient.first_name patient.last_name patient.gender patient.phone patient.address patient.insurance_id from patient where patient.patient_no = @param; select medication.med_code medication.name medication.brand medication.description from medication where medication.name like %param; select medication.med_code medication.name medication.brand medication.description from medication where medication.brand like %param;"
 
 def getQueryParts(query):
     query = query.lower()
@@ -171,8 +174,10 @@ def getFeaturesFromColumns(tablesDic):
 
 # data expected to be a dictionary with the format:
 # {'role': 'HR', 'user': 'bob', 'rows_examined': 4, 'timestamp': '11-24-18 19:44:57', 'text': 'select * from patient'}
-def getFeaturesFromQuery(data):
+# add_BOW = boolean
+def getFeaturesFromQuery(data, add_BOW=False):
     query = data.get("text")
+    #query = "select employees.emp_no from employees where employees.first_name like @param%"
     rows_examined = data.get("rows_examined")
 
     command, tablesAndColumns, tablesAndAttributes = getQueryParts(query)
@@ -185,22 +190,45 @@ def getFeaturesFromQuery(data):
     arrayColumns = arrayColumns.flatten()
     arrayAttributes = arrayAttributes.flatten()
     
-    retval = np.concatenate([arrayCommand, arrayTables, arrayColumns, arrayAttributes, np.array([rows_examined])])
+    arrayBow = []
+    if (add_BOW):
+        vectorizer = CountVectorizer(stop_words=None, min_df=0) # ngram_range=(1, 2)
+        bow_ = BOW.split("; ")        
+        bow_ = np.array(bow_)
+        x = vectorizer.fit_transform(bow_).toarray()
+        #print(bow_)        
+        param = []
+        param.append(query)
+        arrayBow = vectorizer.transform(param).toarray()[0]
+        #print('arrayBow: ', arrayBow.shape)
+
+    retval = np.concatenate([arrayCommand, arrayTables, arrayColumns, arrayAttributes, np.array([rows_examined]), np.array(arrayBow)])
     retval = retval.reshape(1, -1)  #only one sample here
-    # print('Printing arrayCommand: ')
-    # print(arrayCommand)
-    # print('Printing arrayTables: ')
-    # print(arrayTables)
-    # print('Printing arrayColumns: ')
-    # print(arrayColumns)
-    # print('Printing arrayAttributes: ')
-    # print(arrayAttributes)
+    #print('retval ', retval.shape)
     return retval
+
+# we exclude the features from the WHERE statemnt
+def getFeaturesFromDBSAFE(data):
+    query = data.get("text")
+    rows_examined = data.get("rows_examined")
+
+    command, tablesAndColumns, _ = getQueryParts(query)
+
+    arrayCommand = getFeaturesFromCommand(command)
+    arrayTables = getFeaturesFromTables(tablesAndColumns)
+    arrayColumns = getFeaturesFromColumns(tablesAndColumns)
+
+    arrayColumns = arrayColumns.flatten()
+    
+    retval = np.concatenate([arrayCommand, arrayTables, arrayColumns, np.array([rows_examined])])
+    retval = retval.reshape(1, -1)  #only one sample here
+
+    return retval    
 
 
 if __name__ == "__main__":
     print('Holi - ', TEST_QUERY_SELECT_ALL)
-    features = getFeaturesFromQuery(TEST_DATA)
+    features = getFeaturesFromQuery(TEST_DATA, True)
     print('FINAL FEATURES: ')
     print(features)
     # result = getQueryParts(TEST_QUERY_SELECT_ALL)
